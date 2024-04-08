ef7f0e06cee9b7cee63ab00642b55841b86cee2e
AnotherDayOfTrying <javierjustin7@gmail.com>
2024-04-07 23:05:34 -0600

    integrity

--- file.ts
+++ file.ts
@@ -26,14 +26,17 @@ export const updateMetadata = async (client: MongoClient, metadata: Metadata) =>
     await client.db('sfs').collection<Metadata>('metadata').updateOne({name: metadata.name}, metadata)
 }
 
-export const verifyUserFiles = async (client: MongoClient, user: string, pwd: string) => {
+export const verifyUserFiles = async (client: MongoClient, user: string, root_dir: string) => {
     const _user = await client.db('sfs').collection<User>('users').findOne({_id: new ObjectId(user)})
-    console.log(_user)
-    console.log(pwd)
+    if (!_user) {
+        return
+    }
+    const pwd = path.join(root_dir, _user.username)
     await Promise.all(fs.readdirSync(pwd, {withFileTypes: true, recursive: true}).map(async (file) => {
         if (!file.isDirectory()) {
             const metadata = await fetchMetadata(client, file.name)
             const data = fs.readFileSync(path.join(pwd, file.name)).toString()
+            console.log(hashFileIntegrity(file.name, data))
             console.log(file)
             console.log(metadata)
             console.log(data)
