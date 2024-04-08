68c0cbb8b5e63f3f9e3aae56347e290028d7161e
AnotherDayOfTrying <javierjustin7@gmail.com>
2024-04-07 18:49:55 -0600

    encryption on users

--- encryption.ts
+++ encryption.ts
@@ -1,20 +1,29 @@
 import crypto from 'crypto';
 
 const algorithm = 'aes-256-ctr';
-const password = 'password'; 
-const secretKey = crypto.createHash('sha256').update(String(password)).digest('base64').substr(0, 32);
-const iv = crypto.randomBytes(16);
+// const password = 'password'; 
+// const secretKey = crypto.createHash('sha256').update(String(password)).digest('base64').substr(0, 32);
+// const iv = crypto.randomBytes(16);
 
-export const encrypt = (buffer: Buffer): Buffer => {
+
+export const generateKey = (password: string): string => {
+    return crypto.createHash('sha256').update(password).digest('base64').substr(0, 32);
+}
+
+export const generateIV = (): string => {
+    return crypto.randomBytes(16).toString('utf-8');
+}
+
+export const encrypt = (buffer: Buffer, secretKey: string, iv: string): Buffer => {
   const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
-  const result = Buffer.concat([iv, cipher.update(buffer), cipher.final()]);
+  const result = Buffer.concat([Buffer.from(iv), cipher.update(buffer), cipher.final()]);
   return result;
 };
 
-export const decrypt = (encrypted: Buffer): Buffer => {
+export const decrypt = (encrypted: string, secretKey: string): Buffer => {
   const iv = encrypted.slice(0, 16);
   const data = encrypted.slice(16);
   const decipher = crypto.createDecipheriv(algorithm, secretKey, iv);
-  const result = Buffer.concat([decipher.update(data), decipher.final()]);
+  const result = Buffer.concat([decipher.update(Buffer.from(data)), decipher.final()]);
   return result;
 };
