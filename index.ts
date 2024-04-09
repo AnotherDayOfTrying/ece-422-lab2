e8ee195523f062a62f890719dbdbf9c93529cec4
AnotherDayOfTrying <javierjustin7@gmail.com>
2024-04-08 19:41:09 -0600

    fixed

--- index.ts
+++ index.ts
@@ -166,7 +166,7 @@ await yargs(process.argv.slice(2))
           console.error("Metadata not found for file")
           return
         }
-        const fileName = decryptWithPermission(client, Buffer.from(file.name, 'hex'), userInfo, metadata.read)
+        const fileName = await decryptWithPermission(client, Buffer.from(file.name, 'hex'), userInfo, metadata.read)
         console.log("filename")
         
         console.log(file.isDirectory() ? "/" + fileName : fileName)
